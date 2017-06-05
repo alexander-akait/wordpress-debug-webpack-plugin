2ea7162fdd45bf23ede92f93f504d38273362372
evilebottnawi <alexander.krasnoyarov@itgalaxy.company>
2017-06-05 23:21:17 +0300

    Style: integrate `prettier`.

--- src/WordpressDebugWebpackPlugin.js
+++ src/WordpressDebugWebpackPlugin.js
@@ -7,10 +7,14 @@ export default class WordpressDebugWebpackPlugin {
             throw new Error('Path to `wp-config.php` is required');
         }
 
-        this.options = Object.assign({}, {
-            debug: true,
-            runOnce: true
-        }, options);
+        this.options = Object.assign(
+            {},
+            {
+                debug: true,
+                runOnce: true
+            },
+            options
+        );
         this.doneOnRun = false;
         this.doneOnWatch = false;
     }
@@ -18,28 +22,26 @@ export default class WordpressDebugWebpackPlugin {
     apply(compiler) {
         const { options } = this;
         const events = ['run', 'watch-run'];
-        const capitalizeFirstLetter = (string) => string.charAt(0).toUpperCase() + string.slice(1);
+        const capitalizeFirstLetter = string =>
+            string.charAt(0).toUpperCase() + string.slice(1);
 
-        events.forEach((event) => {
-            compiler.plugin(
-                event,
-                (compilerInstance, callback) => {
-                    const doneOptionName = `doneOn${capitalizeFirstLetter(event)}`;
+        events.forEach(event => {
+            compiler.plugin(event, (compilerInstance, callback) => {
+                const doneOptionName = `doneOn${capitalizeFirstLetter(event)}`;
 
-                    if (this.options.runOnce && this[doneOptionName]) {
-                        return callback();
-                    }
-
-                    return nodeify(
-                        wordpressDebug(options.wpConfigPath, options.debug),
-                        (error) => {
-                            this[doneOptionName] = true;
-
-                            return callback(error);
-                        }
-                    );
+                if (this.options.runOnce && this[doneOptionName]) {
+                    return callback();
                 }
-            );
+
+                return nodeify(
+                    wordpressDebug(options.wpConfigPath, options.debug),
+                    error => {
+                        this[doneOptionName] = true;
+
+                        return callback(error);
+                    }
+                );
+            });
         });
     }
 }
--- src/__tests__/WordpressDebugWebpackPlugin-test.js
+++ src/__tests__/WordpressDebugWebpackPlugin-test.js
@@ -4,21 +4,18 @@ import path from 'path';
 import pify from 'pify'; // eslint-disable-line node/no-unpublished-import
 import tempy from 'tempy'; // eslint-disable-line node/no-unpublished-import
 import test from 'ava'; // eslint-disable-line node/no-unpublished-import
-import webpack from 'webpack';
+import webpack from 'webpack'; // eslint-disable-line node/no-unpublished-import
 import webpackConfigBase from './fixtures/config-base';
 
 const fixturesDir = path.resolve(__dirname, 'fixtures');
 
-test(
-    'should throw error if not passed path to `wp-config.php`',
-    (t) => {
-        t.throws(() => new WordpressDebugWebpackPlugin());
-    }
-);
+test('should throw error if not passed path to `wp-config.php`', t => {
+    t.throws(() => new WordpressDebugWebpackPlugin());
+});
 
 test.serial(
     'should completed successfully when option `debug` is not set',
-    (t) => {
+    t => {
         const tmpPath = tempy.directory();
         const wpConfigPath = path.join(fixturesDir, 'wp-config.php');
 
@@ -30,23 +27,24 @@ test.serial(
             })
         ];
 
-        return pify(webpack)(webpackConfigBase)
-            .then((stats) => {
-                t.true(stats.compilation.errors.length === 0, 'no compilation error');
+        return pify(webpack)(webpackConfigBase).then(stats => {
+            t.true(
+                stats.compilation.errors.length === 0,
+                'no compilation error'
+            );
 
-                return pify(fs).readFile(wpConfigPath, 'utf8')
-                    .then((data) => {
-                        t.true(/define\('WP_DEBUG',\strue\)/.test(data));
+            return pify(fs).readFile(wpConfigPath, 'utf8').then(data => {
+                t.true(/define\('WP_DEBUG',\strue\)/.test(data));
 
-                        return data;
-                    });
+                return data;
             });
+        });
     }
 );
 
 test.serial(
     'should completed successfully when option `debug` is set to `true`',
-    (t) => {
+    t => {
         const tmpPath = tempy.directory();
         const wpConfigPath = path.join(fixturesDir, 'wp-config.php');
 
@@ -59,23 +57,24 @@ test.serial(
             })
         ];
 
-        return pify(webpack)(webpackConfigBase)
-            .then((stats) => {
-                t.true(stats.compilation.errors.length === 0, 'no compilation error');
+        return pify(webpack)(webpackConfigBase).then(stats => {
+            t.true(
+                stats.compilation.errors.length === 0,
+                'no compilation error'
+            );
 
-                return pify(fs).readFile(wpConfigPath, 'utf8')
-                    .then((data) => {
-                        t.true(/define\('WP_DEBUG',\strue\)/.test(data));
+            return pify(fs).readFile(wpConfigPath, 'utf8').then(data => {
+                t.true(/define\('WP_DEBUG',\strue\)/.test(data));
 
-                        return data;
-                    });
+                return data;
             });
+        });
     }
 );
 
 test.serial(
     'should completed successfully when option `debug` is set to `false`',
-    (t) => {
+    t => {
         const tmpPath = tempy.directory();
         const wpConfigPath = path.join(fixturesDir, 'wp-config.php');
 
@@ -88,23 +87,24 @@ test.serial(
             })
         ];
 
-        return pify(webpack)(webpackConfigBase)
-            .then((stats) => {
-                t.true(stats.compilation.errors.length === 0, 'no compilation error');
+        return pify(webpack)(webpackConfigBase).then(stats => {
+            t.true(
+                stats.compilation.errors.length === 0,
+                'no compilation error'
+            );
 
-                return pify(fs).readFile(wpConfigPath, 'utf8')
-                    .then((data) => {
-                        t.true(/define\('WP_DEBUG',\sfalse\)/.test(data));
+            return pify(fs).readFile(wpConfigPath, 'utf8').then(data => {
+                t.true(/define\('WP_DEBUG',\sfalse\)/.test(data));
 
-                        return data;
-                    });
+                return data;
             });
+        });
     }
 );
 
 test.serial(
     'should completed successfully when option `debug` is set to `true` and `runOnce` is set to `true`',
-    (t) => {
+    t => {
         const tmpPath = tempy.directory();
         const wpConfigPath = path.join(fixturesDir, 'wp-config.php');
 
@@ -118,23 +118,24 @@ test.serial(
             })
         ];
 
-        return pify(webpack)(webpackConfigBase)
-            .then((stats) => {
-                t.true(stats.compilation.errors.length === 0, 'no compilation error');
+        return pify(webpack)(webpackConfigBase).then(stats => {
+            t.true(
+                stats.compilation.errors.length === 0,
+                'no compilation error'
+            );
 
-                return pify(fs).readFile(wpConfigPath, 'utf8')
-                    .then((data) => {
-                        t.true(/define\('WP_DEBUG',\strue\)/.test(data));
+            return pify(fs).readFile(wpConfigPath, 'utf8').then(data => {
+                t.true(/define\('WP_DEBUG',\strue\)/.test(data));
 
-                        return data;
-                    });
+                return data;
             });
+        });
     }
 );
 
 test.serial(
     'should completed successfully when option `debug` is set to `true` and `runOnce` is set to `false`',
-    (t) => {
+    t => {
         const tmpPath = tempy.directory();
         const wpConfigPath = path.join(fixturesDir, 'wp-config.php');
 
@@ -148,23 +149,24 @@ test.serial(
             })
         ];
 
-        return pify(webpack)(webpackConfigBase)
-            .then((stats) => {
-                t.true(stats.compilation.errors.length === 0, 'no compilation error');
+        return pify(webpack)(webpackConfigBase).then(stats => {
+            t.true(
+                stats.compilation.errors.length === 0,
+                'no compilation error'
+            );
 
-                return pify(fs).readFile(wpConfigPath, 'utf8')
-                    .then((data) => {
-                        t.true(/define\('WP_DEBUG',\strue\)/.test(data));
+            return pify(fs).readFile(wpConfigPath, 'utf8').then(data => {
+                t.true(/define\('WP_DEBUG',\strue\)/.test(data));
 
-                        return data;
-                    });
+                return data;
             });
+        });
     }
 );
 
 test.serial(
     'should completed successfully when option `debug` is set to `false` and `runOnce` is set to `true`',
-    (t) => {
+    t => {
         const tmpPath = tempy.directory();
         const wpConfigPath = path.join(fixturesDir, 'wp-config.php');
 
@@ -178,23 +180,24 @@ test.serial(
             })
         ];
 
-        return pify(webpack)(webpackConfigBase)
-            .then((stats) => {
-                t.true(stats.compilation.errors.length === 0, 'no compilation error');
+        return pify(webpack)(webpackConfigBase).then(stats => {
+            t.true(
+                stats.compilation.errors.length === 0,
+                'no compilation error'
+            );
 
-                return pify(fs).readFile(wpConfigPath, 'utf8')
-                    .then((data) => {
-                        t.true(/define\('WP_DEBUG',\sfalse\)/.test(data));
+            return pify(fs).readFile(wpConfigPath, 'utf8').then(data => {
+                t.true(/define\('WP_DEBUG',\sfalse\)/.test(data));
 
-                        return data;
-                    });
+                return data;
             });
+        });
     }
 );
 
 test.serial(
     'should completed successfully when option `debug` is set to `false` and `runOnce` is set to `false`',
-    (t) => {
+    t => {
         const tmpPath = tempy.directory();
         const wpConfigPath = path.join(fixturesDir, 'wp-config.php');
 
@@ -208,16 +211,17 @@ test.serial(
             })
         ];
 
-        return pify(webpack)(webpackConfigBase)
-            .then((stats) => {
-                t.true(stats.compilation.errors.length === 0, 'no compilation error');
+        return pify(webpack)(webpackConfigBase).then(stats => {
+            t.true(
+                stats.compilation.errors.length === 0,
+                'no compilation error'
+            );
 
-                return pify(fs).readFile(wpConfigPath, 'utf8')
-                    .then((data) => {
-                        t.true(/define\('WP_DEBUG',\sfalse\)/.test(data));
+            return pify(fs).readFile(wpConfigPath, 'utf8').then(data => {
+                t.true(/define\('WP_DEBUG',\sfalse\)/.test(data));
 
-                        return data;
-                    });
+                return data;
             });
+        });
     }
 );
